f576551dd4faa7d3f2ac244b4eff3fbe93071de5
etthann <110430934+etthann@users.noreply.github.com.>
2025-01-12 00:53:43 -0500

    Added Registration Logic

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,17 +1,35 @@
 import React, { useState } from 'react';
 import '../styles/globals.css';
+import axios from 'axios';
+import { useRouter } from 'next/router';
 
 const RegisterPage: React.FC = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [name, setName] = useState('');
+    const router = useRouter();
 
-
-    const handleRegister = (e: React.FormEvent) => {
+    const handleRegister = async (e: React.FormEvent) => {
         e.preventDefault();
         // Handle login logic here
-
-
-
+        try {
+            const response = await axios.post('http://localhost:5000/register', {
+                name,
+                email,
+                password,
+            });
+            
+            if (response.status === 200) {
+                // Handle successful registration
+                console.log('Registration successful');
+                router.push('/');
+            } else {
+                // Handle registration error
+                console.error('Registration failed');
+            }
+        } catch (error) {
+            console.error('An error occured during registration: ', error);
+        }
 
     };
 
@@ -19,13 +37,25 @@ const RegisterPage: React.FC = () => {
         <div className="flex h-screen w-screen items-center justify-center bg-gray-50">
             <div className="z-10 w-full max-w-md overflow-hidden rounded-2xl border border-gray-100 shadow-xl">
                 <div className="flex flex-col items-center justify-center space-y-3 border-b border-gray-200 bg-white px-4 py-6 pt-8 text-center sm:px-16">
-                    <h3 className="text-xl font-semibold">Sign In</h3>
+                    <h3 className="text-xl font-semibold">Sign Up</h3>
                     <p className="text-sm text-gray-500">
-                        Use your email and password to sign in
+                        Enter your name, email and create a password!
                     </p>
                 </div>
 
                 <form onSubmit={handleRegister} className="max-w-sm mx-auto px-4 py-6 sm:px-16">
+                    <div className="mb-5">
+                        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Your Full name</label>
+                        <input
+                            type="text"
+                            id="text"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="Enter your full name"
+                            required
+                        />
+                    </div>
                     <div className="mb-5">
                         <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Your email</label>
                         <input
